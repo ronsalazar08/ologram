03ed9d67bc5b4b55900b54898f84972a5b871078
ron <ronsalazar01@gmail.com>
2023-10-15 00:23:38 +0800

    change whole env

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ron/Documents/Projects/ologram/env"
+set -gx VIRTUAL_ENV "/Users/ron/Documents/GitHub/ologram/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
