7f5d21e615285fb1c025b73fe58324e45b03f29b
ron <ronsalazar01@gmail.com>
2023-10-14 19:12:14 +0800

    Initial commit

---
+++ ologram_backend/post/api.py
@@ -0,0 +1,50 @@
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from account.models import User
+from account.serializers import UserSerializer
+
+from .forms import PostForm
+from .models import Post
+from .serializers import PostSerializer
+
+
+@api_view(['GET'])
+def post_list(request):
+    posts = Post.objects.all() # Change later to feed
+
+    serializer = PostSerializer(posts, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
+def post_list_profile(request, id):
+    user = User.objects.get(pk=id)
+    posts = Post.objects.filter(created_by_id=id)
+
+    posts_serializer = PostSerializer(posts, many=True)
+    user_serializer = UserSerializer(user)
+
+    return JsonResponse({
+        'posts': posts_serializer.data,
+        'user': user_serializer.data
+    }, safe=False)
+
+
+@api_view(['POST'])
+def post_create(request):
+    form = PostForm(request.data)
+
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.created_by = request.user
+        post.save()
+
+        serializer = PostSerializer(post)
+
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        message = ' '.join([' '.join(x for x in l) for l in list(form.errors.values())])
+        return JsonResponse({'error': message})
